5475f4e16305ba54a6c1fc0bc56a53c0d85ce2cd
Alex <alexkolydas@gmail.com>
2018-12-19 22:16:04 +0100

    Initial commit

---
+++ app/src/main/java/com/kolydas/greeksinbrno/MainActivity.java
@@ -0,0 +1,65 @@
+package com.kolydas.greeksinbrno;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.kolydas.greeksinbrno.specialities.Courses;
+import com.kolydas.greeksinbrno.specialities.Doctors;
+import com.kolydas.greeksinbrno.specialities.Food;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(MainActivity.this);
+
+        loadFragment(new Food()); //Displays the Food fragment by default on bottom navigation
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment= null;
+
+        switch (menuItem.getItemId()){
+
+            case R.id.navigation_food:
+                fragment= new Food();
+                break;
+
+            case R.id.navigation_doctors:
+                fragment= new Doctors();
+                break;
+
+            case R.id.navigation_courses:
+                fragment= new Courses();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
+
+    //Load Fragment
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/kolydas/greeksinbrno/specialities/Food.java
@@ -0,0 +1,20 @@
+package com.kolydas.greeksinbrno.specialities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kolydas.greeksinbrno.R;
+
+public class Food extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_food,null);
+    }
+}
